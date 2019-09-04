0fd4180e904b4746a972efe72375350d22d8e363
Rohit Sinha <rohitsinha4826@gmail.com>
2019-09-05 00:51:08 +0700

    new rohit

---
+++ amar.js
@@ -0,0 +1,16 @@
+var students=[{"name":"Amar","marks":[50,60,30]},{"name":"Birbal","marks":[40,80,65]},{"name":"Rohit","marks":[40,80,65]}];
+
+
+for(var student of students){
+    // var bir=obj[i];
+    // var sum=0;
+    // var avg=0;
+    // for(var j=0;j<bir.marks.length;j++){
+    //     sum=bir.marks[j]+sum;
+    //
+    // }
+    // avg=sum/bir.length;
+
+    console.log(student)
+
+}
