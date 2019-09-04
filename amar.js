525d1dd04433032a17c83d2a8b7aed51d88be525
Rohit Sinha <rohitsinha4826@gmail.com>
2019-09-05 04:44:19 +0700

    new rohit

--- amar.js
+++ amar.js
@@ -1,16 +1,47 @@
-var students=[{"name":"Amar","marks":[50,60,30]},{"name":"Birbal","marks":[40,80,65]},{"name":"Rohit","marks":[40,80,65]}];
+// var students=[{"name":"Amar","marks":[50,60,30]},{"name":"Birbal","marks":[40,80,65]},{"name":"Rohit","marks":[40,80,65]}];
+//
+//
+// for(var student of students){
+//     console.log(student);
+//      var sum=0;
+//      var avg=0;
+//     //
+//     //
+//     for (var mark of student.marks){
+//         console.log(mark)
+//         sum=sum+mark;
+//
+//     }
+//     console.log(sum);
+//     avg=sum/student.marks.length;
+//     console.log(avg);
+//     console.log(student.name + 'scored' +)
+// }
+var students = [{
+    "name": "Amar",
+    "subjects": [{subjectNames: 'english', marks: [10, 20, 40]}, {subjectNames: 'maths', marks: [10, 20, 30]}]
+},
+    {
+        "name": "sdfadsf",
+        "subjects": [{subjectNames: 'english', marks: [10, 20, 20]}, {subjectNames: 'maths', marks: [10, 20, 30]}]
+    }];
 
 
-for(var student of students){
-    // var bir=obj[i];
-    // var sum=0;
-    // var avg=0;
-    // for(var j=0;j<bir.marks.length;j++){
-    //     sum=bir.marks[j]+sum;
-    //
-    // }
-    // avg=sum/bir.length;
+for (var student of students) {
+    // console.dir(student, {depth:null});
 
-    console.log(student)
+    for (var subject of student.subjects) {
+
+        // // //
+        sum = 0;
+        for (var mark of subject.marks) {
+            sum = sum + mark;
+
+
+        }
+        console.log(student.name + 'scored' + sum + 'in' + subject.subjectNames);
+    }
+
 
 }
+
